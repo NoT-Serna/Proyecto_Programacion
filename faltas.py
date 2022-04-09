ec5fc797a5a05ecc058f992d5cfe5064dce295d0
jcph1 <69978753+jcph1@users.noreply.github.com>
2022-04-09 15:01:38 -0500

    Update faltas.py

--- faltas.py
+++ faltas.py
@@ -1,33 +1,31 @@
+import clase
 falta_menor=["intento ataque", "broken stick", "charging", "boarding", "cross checking", "delaying", "elbowing", " falling on the puck","handpass", "high sticks", "holding", "hoocking","interference","slashing","tripping" ]
 falta_mayor=["fighting","interference by spectators", "checking from behind", 'leaving penalty bench', "abuse of officials", "profane language", ]
-jugador_1= int(input("Ingrese el numero de jersey del jugador penalizado: "))
 
+nro_jugador= int(input("Ingrese el numero de jersey del jugador penalizado: "))
 
 def falta_penalizacion():
     x=input("Ingrese el nombre de la falta: ")
     faltas=0
 
     if x in falta_menor:
-        print("Debe permanecer durante 2 minutos en el penalty box")
+        print("El jugador penalizado debe permanecer durante 2 minutos en el penalty box")
         faltas+=1
         
     elif x in falta_mayor:
-        print("Debe permanecer durante 10 minutos en el penalty box")
+        print("El jugador penalizado debe permanecer durante 10 minutos en el penalty box")
         faltas+=3
         
     elif x not in falta_menor or x not in falta_mayor:
         print("la falta no existe o esta escrita de forma incorrecta")
     
     return faltas
+nro_faltas=falta_penalizacion()
+jugador=clase.Jugador(nro_jugador,nro_faltas)
 
-
-def misconduct(falta_penalizacion):
+def misconduct():
     
-    if falta_penalizacion < 2:
-        print("el jugador (format) no esta en riesgo de misconduct")    
+    if jugador.nro_faltas < 2:
+        print("El jugador con numero de jersey",jugador.nro_jersey, "no esta en riesgo de misconduct")    
     else:
-        print("el jugador (format) se debe de expulsar al jugador del partido")
-        
-
-
-#hacer funcion que sume todas las faltas de los jugadores dependiendo del equipo en el que esten 
+        print("El jugador con numero de jersey",jugador.nro_jersey, "se debe de expulsar al jugador del partido")
