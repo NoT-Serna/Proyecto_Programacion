39a679632c272e3ece7b1269431dd03c8588f7e4
jcph1 <69978753+jcph1@users.noreply.github.com>
2022-05-14 21:27:25 -0500

    Update Jugador.py

--- Jugador.py
+++ Jugador.py
@@ -1,7 +1,7 @@
 class Jugador:
     def __init__(self,nro_jersey):
         self.nro_jersey=nro_jersey
-        self.nro_faltas=0
+        self.nro_faltas=0 # empieza el nro_faltas en cero para poder ir aumentandole faltas
     
     def get_nro_jersey(self):
         return self.nro_jersey
@@ -13,7 +13,7 @@ class Jugador:
         self.nro_faltas=new_nro_faltas
         
     def agregar_falta(self):
-        self.nro_faltas += 1
+        self.nro_faltas += 1 # le aumenta el nro_faltas al objeto Jugador
         
     def __str__(self):
         return f"Jersey:{self.nro_jersey} Faltas:{self.nro_faltas}"
